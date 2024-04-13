504d7cb42d3b8c8773fa737b20595ff555591d60
C2122 <d>
2024-04-13 07:29:15 -0700

    new  como

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-print("-------MATH--------")
-var = 1
-var2 = 2
-var3 = 5
-var4 = 7
-var5 = 3
-var6 = 3
+class Student:
+    def  __init__(self,name="none"):
+        self.name = name
 
-print(var + var2)
+    def   __str__(self):
+        return  f'hello, my  name  is {self.name}'
 
-print(var3 - var4)
-
-print(var * var2)
-
-print(var5 * var6)
+    def __del__(self):
+        print("i'm graduate")
+stud = Student('Andriy')
+print(stud)
 
 
 
-#math
-print("-------MATH--------")
+
+
+    #print("hi")
+    #amount_of_students  =  0
+    #def __init__(self, height=0, name="noname",mark=0):
+        #self.height = 160
+        #self.mark=mark
+        #self.name=name
+        #self.height = height
+       # print("Зріст",self.name,":", self.height)
+       # Student.amount_of_students += 1
+
+
+
+    #def  growUp(self):
+      #def  self.height += 10
+
+    #def growMark(self):
+        #self.mark += 10
+
+#maxym = Student(height=160, name = 'maxym')
+#kate = Student(height=170, name = 'kate')
+#stepan = Student(height=169, name = "stepan")
+
+#maxym.growUp()
+#print(maxym.mark)
+#print(maxym.mark)
+
+
+#print(kate.height)
+#print(stepan.height)
+
+
